e2e359680ae23625fc2276fb69959f5e0204b6fa
Steven Hertz <hertzsteven@icloud.com>
2023-02-16 20:36:59 -0500

    Initial Commit

---
+++ addAppsToCloudkit/addAppsToCloudkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  addAppsToCloudkitApp.swift
+//  addAppsToCloudkit
+//
+//  Created by Steven Hertz on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct addAppsToCloudkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
